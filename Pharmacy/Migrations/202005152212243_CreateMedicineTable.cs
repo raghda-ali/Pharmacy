97c92f00182c69978e7e8069665a279f88be2cb6
raghda-ali <raghdaali991@gmail.com>
2020-05-16 00:12:59 +0200

    new migration for medicine table

---
+++ Pharmacy/Migrations/202005152212243_CreateMedicineTable.cs
@@ -0,0 +1,16 @@
+﻿namespace Pharmacy.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateMedicineTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
