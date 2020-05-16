c672d36bb9407d625cb11759cf7e1f6f9b02f264
raghda-ali <raghdaali991@gmail.com>
2020-05-16 17:09:08 +0200

    update pharmacist table

---
+++ Pharmacy/Controllers/LoginController.cs
@@ -0,0 +1,65 @@
+﻿using Pharmacy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pharmacy.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult LoginPharmacist(Pharmacy.Models.Pharmacist pharmacist)
+        {
+            using (MyDBContext db = new MyDBContext())
+            {
+                var pharmacistdetails = db.pharmacists.Where(x => x.Username == pharmacist.Username && x.Password == pharmacist.Password).FirstOrDefault();
+                if (pharmacistdetails == null)
+                {
+                    return View("LoginPharmacist", pharmacist);
+                }
+                else
+                {
+                    Session["id"] = pharmacist.id;
+                    return RedirectToAction("Contact", "About");
+                }
+            }
+        }
+    }
+}
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Login(Pharmacist objUser)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        using (MyDBContext db = new MyDBContext())
+        //        {
+        //            var obj = db.pharmacists.Where(a => a.Username.Equals(objUser.Username) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+        //            if (obj != null)
+        //            {
+        //                Session["id"] = obj.id.ToString();
+        //                Session["Username"] = obj.Username.ToString();
+        //                return RedirectToAction("UserDashBoard");
+        //            }
+        //        }
+        //    }
+        //    return View(objUser);
+        //}
+
+        //public ActionResult UserDashBoard()
+        //{
+        //    if (Session["idservese"] != null)
+        //    {
+        //        return View();
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction("Login");
+        //    }
+      
--- Pharmacy/Models/Pharmacist.cs
+++ Pharmacy/Models/Pharmacist.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +10,16 @@ namespace Pharmacy.Models
     public class Pharmacist
     {
         public int id { get; set; }
+        [DisplayName("User Name")]
+        [Required(ErrorMessage ="This field is required.")]
         public string Username { get; set; }
+        [DisplayName("Password")]
         public string Password { get; set; }
+        [DisplayName("First Name")]
         public string Firstname { get; set; }
+        [DisplayName("Last Name")]
         public string Lastname { get; set; }
+        [DisplayName("Email")]
         public string Email { get; set; }
     }
 }
