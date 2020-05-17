d57ee8ea28e4e13edbde2f18822fba1c5d2611be
ririelgamal <ririelgamal@gmail.com>
2020-05-17 14:02:03 +0400

    delete medicine function

---
+++ Pharmacy/Controllers/DeleteMedicinesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pharmacy.Models;
+
+namespace Pharmacy.Controllers
+{
+    public class DeleteMedicinesController : Controller
+    {
+        private MyDBContext db = new MyDBContext();
+
+        // GET: DeleteMedicines
+        public ActionResult Index()
+        {
+            return View(db.medicines.ToList());
+        }
+
+        // GET: DeleteMedicines/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicine medicine = db.medicines.Find(id);
+            if (medicine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicine);
+        }
+
+        // GET: DeleteMedicines/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: DeleteMedicines/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "id,name,quantity,price")] Medicine medicine)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.medicines.Add(medicine);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(medicine);
+        //}
+
+        //// GET: DeleteMedicines/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Medicine medicine = db.medicines.Find(id);
+        //    if (medicine == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(medicine);
+        //}
+
+        //// POST: DeleteMedicines/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "id,name,quantity,price")] Medicine medicine)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(medicine).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(medicine);
+        //}
+
+        // GET: DeleteMedicines/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicine medicine = db.medicines.Find(id);
+            if (medicine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicine);
+        }
+
+        // POST: DeleteMedicines/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Medicine medicine = db.medicines.Find(id);
+            db.medicines.Remove(medicine);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
