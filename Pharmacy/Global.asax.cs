0d210b963638e9f7e5b5b96c676905561f67f37f
ranawageh4@outlook.com <ranawageh4@outlook.com>
2020-05-18 01:27:01 +0200

    create update pharmacist

--- Pharmacy/Global.asax.cs
+++ Pharmacy/Global.asax.cs
@@ -5,8 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-//using System.Data.Entity;
-//using Pharmacy.Models;
+
 namespace Pharmacy
 {
     public class MvcApplication : System.Web.HttpApplication
@@ -17,7 +16,6 @@ namespace Pharmacy
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            //Database.SetInitializer<MyDBContext>(new DropCreateDatabaseIfModelChanges<MyDBContext>());
         }
     }
 }
