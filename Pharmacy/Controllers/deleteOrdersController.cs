28fdf9671eae57fb467f9c4963e5bbe3db86cfa4
ririelgamal <ririelgamal@gmail.com>
2020-05-29 20:39:08 +0400

    delete order function

---
+++ Pharmacy/Controllers/deleteOrdersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pharmacy.Models;
+
+namespace Pharmacy.Controllers
+{
+    public class deleteOrdersController : Controller
+    {
+        private MyDBContext db = new MyDBContext();
+
+        // GET: deleteOrders
+        public ActionResult Index()
+        {
+            var orders = db.orders.Include(o => o.Medicine);
+            return View(orders.ToList());
+        }
+
+        // GET: deleteOrders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: deleteOrders/Create
+        public ActionResult Create()
+        {
+            ViewBag.medicine_id = new SelectList(db.medicines, "id", "name");
+            return View();
+        }
+
+        // POST: deleteOrders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "medicine_id,total_price,amount")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.medicine_id = new SelectList(db.medicines, "id", "name", order.medicine_id);
+            return View(order);
+        }
+
+        // GET: deleteOrders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.medicine_id = new SelectList(db.medicines, "id", "name", order.medicine_id);
+            return View(order);
+        }
+
+        // POST: deleteOrders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "medicine_id,total_price,amount")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.medicine_id = new SelectList(db.medicines, "id", "name", order.medicine_id);
+            return View(order);
+        }
+
+        // GET: deleteOrders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: deleteOrders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.orders.Find(id);
+            db.orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
